3c59ea1c80abefe448961c9a6381e32b9aa8c0c2
Salvatore <marcofornaro33@gmail.com>
2019-12-27 14:16:15 +0100

    j7xelte: exynos7870: BT configs

--- device.mk
+++ device.mk
@@ -206,7 +206,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-impl.7870 \
     libbt-vendor
 
 # Audio
